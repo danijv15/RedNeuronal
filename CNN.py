95b3a1bbbf4453a64d0805d56066e29c2c00f117
Daniel Jara Vargas <danijv-92@hotmail.com>
2019-06-16 12:49:10 -0600

    Archivo Inicial

---
+++ CNN.py
@@ -0,0 +1,33 @@
+# Se importan librerias
+
+import tensorflow as tf
+import numpy as np
+# Cambiar el import
+from tensorflow.examples.tutorials.mnist import input_data
+import os
+
+#----------------------------------------------------------------------
+# Se importan los datos
+
+# Cambiar ubicación de dataset
+data = input_data.read_data_sets('data/fashion', one_hot=True)
+
+#----------------------------------------------------------------------
+#Se analizan los datos
+
+#----------------------------------------------------------------------
+#Pre procesamiento de los datos
+
+#----------------------------------------------------------------------
+#Parámetros de la red
+
+#----------------------------------------------------------------------
+#Entrenamiento de la red
+
+
+#----------------------------------------------------------------------
+#Capa de red convolusional
+
+
+#----------------------------------------------------------------------
+#Parametros de red
